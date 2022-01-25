c46049c3d8656ec79e843212ce34658c6ade2b08
TA <appulegugure@gmail.com>
2022-01-25 14:33:01 +0900

    少し修正しました。 見づらいかもしれないですがわざとです。

--- B課題/beautiful_kuku.py
+++ B課題/beautiful_kuku.py
@@ -4,8 +4,8 @@ if row <= 0:
 colum = int(input("列数を入力して下さい"))
 if colum <= 0:
     raise ValueError("cloum <= 0 はエラー")
-base = [i * 1 for i in range(1, colum + 1)]
 for y in range(1, row + 1):
     result = ''.join([f"{int(i) / y:.0f} × {str(y)} = " + i + " | " if len(
-        i) > 1 else f"{int(i) / y:.0f} × {str(y)} = " + " " + i + " | " for i in [str(i * y) for i in base]])
+        i) > 1 else f"{int(i) / y:.0f} × {str(y)} = " + " " + i + " | " for i in
+                      [str(z * y) for z in [g for g in range(1, colum + 1)]]])
     print(result)
